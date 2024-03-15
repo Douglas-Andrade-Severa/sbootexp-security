1af9ebc4be7d7ff48b2a68d5fccc9235e36912ab
Douglas Andrade Severa <douglas.severa@eximioti.com.br>
2024-03-15 13:46:35 -0300

    Curso Udemy

---
+++ sbootexp-security/src/main/java/io/girhub/curso/sbootexpsecurity/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package io.girhub.curso.sbootexpsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http, PasswordMasterAuthenticationProvider passwordMasterAuthenticationProvider, CustomFilter customFilter) throws Exception{
+        // role -> grupo de usuário (perfil) -> master, gerente, vendedor
+        // authority -> permissões -> cadastrar usuário, acessar tela consulta, acessar tela de relatório
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(customizer -> {
+                    customizer.requestMatchers("/public").permitAll();
+                    customizer.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .authenticationProvider(passwordMasterAuthenticationProvider)
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails commonUser = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(commonUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults(){
+        return new GrantedAuthorityDefaults("");
+    }
+}
